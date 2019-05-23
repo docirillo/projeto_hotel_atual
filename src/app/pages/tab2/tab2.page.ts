fd872807afe6ea8fbddca4c3323470c1cd93d2a2
NOVOTEL Itu IT <H9399-IT@accor.com>
2019-05-23 15:58:34 -0300

    inclusao do mapa

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentInit, ViewChild } from '@angular/core';
+declare var google;
 
 @Component({
   selector: 'app-tab2',
   templateUrl: './tab2.page.html',
   styleUrls: ['./tab2.page.scss'],
 })
-export class Tab2Page implements OnInit {
+export class Tab2Page implements OnInit, AfterContentInit {
+  map;
+  @ViewChild('mapElement') mapElement;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterContentInit(): void {
+    this.map = new google.maps.Map(
+        this.mapElement.nativeElement,
+        {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
   }
 
 }
