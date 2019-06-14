824483ddab63f0c133e3cd19dcc9a74baf1ac2ae
Daniel Cirillo <daniel.orlandi@outlook.com>
2019-06-13 23:45:49 -0300

    alteracao noticias e atividades

--- src/app/pages/noticias/noticias.page.ts
+++ src/app/pages/noticias/noticias.page.ts
@@ -7,6 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoticiasPage implements OnInit {
 
+  cards = [
+    {
+      title: 'Novotel Itu Golf & Resort',
+      image: '/../assets/img/piscina.jpg',
+      description: 'Aqui você pode conhecer um pouco mais sobre o Hotel, atividades e serviços oferecidos.',
+    },
+    {
+      title: 'Jantar Dia dos Namorados',
+      image: '/../assets/img/news1.png',
+      description: 'Venha comemorar a noite mais romântica do ano no Novotel Itu!!!!',
+      date: '12/06',
+    },
+    {
+      title: 'Feriado de Corpus Christi',
+      image: '/../assets/img/news2.png',
+      description: 'Venha aproveitar o clima junino no Novotel Itu, com uma programação especial, muita festa e comidinhas típicas!',
+      date: '20/06',
+    },
+    {
+      title: 'Atrações Circenses',
+      image: '/../assets/img/news3.png',
+      description: 'O circo vem aí, trazendo um grande espetáculo ao Novotel Itu!',
+      date: 'JULHO',
+    }        
+  ];
+
 
   constructor() { }
 
